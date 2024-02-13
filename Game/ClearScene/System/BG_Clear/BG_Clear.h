0b193c3569c69d184e9296f008078c97f49d0bef
NakamuraRyo0304 <shino.nakiri@gmail.com>
2024-02-14 01:07:05 +0900

    コンパイルデータスコアを無視

--- Game/ClearScene/System/BG_Clear/BG_Clear.h
+++ Game/ClearScene/System/BG_Clear/BG_Clear.h
@@ -4,3 +4,31 @@
  *  @Date   2024-02-14
  *  @Author NakamuraRyo
  */
+
+#pragma once
+#ifndef BG_CLEAR
+#define BG_CLEAR
+
+class BG_Clear
+{
+private:
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="引数無し"></param>
+	/// <returns>なし</returns>
+	BG_Clear();
+	~BG_Clear();
+
+	/// <summary>
+	/// 初期化関数
+	/// </summary>
+	/// <param name="引数無し"></param>
+	/// <returns>なし</returns>
+	void Initialize();
+};
+
+#endif // BG_CLEAR
