93b729f6b6d1461cb8e6bd94b09429fe9d9c3953
NakamuraRyo0304 <shino.nakiri@gmail.com>
2024-04-17 10:44:07 +0900

    コモンシステムの記述を変更

--- Game/Common/DrawKeys/DrawKeys.h
+++ Game/Common/DrawKeys/DrawKeys.h
@@ -12,48 +12,33 @@
 class DrawSprite;
 class DrawKeys
 {
-private:
-
-	// スプライト
-	std::unique_ptr<DrawSprite> m_sprite;
-
-	// 座標
-	DirectX::SimpleMath::Vector2 m_position;
-
-	// 画面拡大率、拡大率
-	DirectX::SimpleMath::Vector2 m_scRate, m_rate;
-
-	// 色
-	DirectX::SimpleMath::Vector4 m_color;
-
-public:
-
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	/// <param name="path">画像パス</param>
-	/// <param name="pos">座標</param>
-	/// <param name="screenRate">画面比率</param>
-	/// <returns>なし</returns>
-	DrawKeys(const wchar_t* path, const DirectX::SimpleMath::Vector2& pos, const DirectX::SimpleMath::Vector2& screenRate);
-	~DrawKeys();
-
-	/// <summary>
-	/// 描画処理
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	void Draw();
-
 public:
 
 	// 色の設定
-	void SetColor(const DirectX::SimpleMath::Vector4& color){ m_color = color; }
+	void SetColor(const DirectX::SimpleMath::Vector4& color) { m_color = color; }
 
 	// 拡大率の取得
 	const DirectX::SimpleMath::Vector2& GetRate() { return m_rate; }
 	// 拡大率の設定
 	void SetRate(const DirectX::SimpleMath::Vector2& rate) { m_rate = rate; }
+
+	// コンストラクタ
+	DrawKeys(const wchar_t* path, const DirectX::SimpleMath::Vector2& pos, const DirectX::SimpleMath::Vector2& screenRate);
+	// デストラクタ
+	~DrawKeys();
+	// 描画
+	void Draw();
+
+private:
+
+	// スプライト
+	std::unique_ptr<DrawSprite> m_sprite;
+	// 座標
+	DirectX::SimpleMath::Vector2 m_position;
+	// 画面拡大率、拡大率
+	DirectX::SimpleMath::Vector2 m_scRate, m_rate;
+	// 色
+	DirectX::SimpleMath::Vector4 m_color;
 
 };
 
--- Game/Common/DrawNumber/DrawNumber.cpp
+++ Game/Common/DrawNumber/DrawNumber.cpp
@@ -8,21 +8,18 @@
 #include "pch.h"
 #include "DrawNumber.h"
 
-//==============================================================================
 // 定数の設定
-//==============================================================================
 const UINT DrawNumber::SIZE = 128;		// １数字分のサイズ
 
-//==============================================================================
 // コンストラクタ
-//==============================================================================
 DrawNumber::DrawNumber()
-	: m_position{}						// 座標
-	, m_origin{}						// 中心座標
-	, m_rate{}							// 文字拡大率
-	, m_screenRate{}					// 画面拡大率
-	, m_rect{}							// 切り取り位置
-	, m_color{}							// 色
+	:
+	m_position(),				// 座標
+	m_origin(),					// 中心座標
+	m_rate(),					// 文字拡大率
+	m_screenRate(),				// 画面拡大率
+	m_rect(),					// 切り取り位置
+	m_color()					// 色
 {
 	m_sprite = std::make_unique<DrawSprite>();
 	m_sprite->MakeSpriteBatch();
@@ -35,70 +32,64 @@ DrawNumber::DrawNumber()
 	m_rate = SimpleMath::Vector2::One;
 }
 
-//==============================================================================
 // デストラクタ
-//==============================================================================
 DrawNumber::~DrawNumber()
 {
 	m_sprite.reset();
 }
 
-//==============================================================================
-// 数字を描画する
-//==============================================================================
+// 描画
 void DrawNumber::Draw(UINT num, Digits digits)
 {
-	UINT _tmp = num;
+	UINT number = num;
 
 	// 描画する桁数を計算
 	if (digits == Digits::One)
 	{
-		_tmp = num % 10;
-		if (_tmp < 1) _tmp = 0;
+		number = num % 10;
+		if (number < 1) number = 0;
 	}
 	if (digits == Digits::Ten)
 	{
-		_tmp = num / 10;
+		number = num / 10;
 
-		if (_tmp < 1) _tmp = 0;
+		if (number < 1) number = 0;
 	}
 
 	// 数字を切り取る
-	m_rect = Cut(_tmp);
+	m_rect = Cut(number);
 
 	// 数字を描画
 	m_sprite->DrawTexture(L"num", m_position * m_screenRate,
 		m_color, m_screenRate * m_rate, m_origin, m_rect);
 }
 
-//==============================================================================
 // 数字を切り取る
-//==============================================================================
 RECT_U DrawNumber::Cut(UINT num)
 {
-	RECT_U _rect;
-	UINT _tmpNum = num;
+	RECT_U rect;
+	UINT number = num;
 
 	// 数字が5-9の場合は下段
-	if (_tmpNum >= 5)
+	if (number >= 5)
 	{
-		_rect.top = SIZE;
-		_rect.bottom = SIZE * 2;			// 上下の切り分け
-		_tmpNum -= 5;						// ０－４の切り分け処理に変換
-											// 例）numが6の場合は1の処理
+		rect.top = SIZE;
+		rect.bottom = SIZE * 2;			// 上下の切り分け
+		number -= 5;					// ０－４の切り分け処理に変換
+										// 例）numが6の場合は1の処理
 		// 左右の切り分け処理
-		_rect.left = _tmpNum * SIZE;		// 左右を切り分け
-		_rect.right = _rect.left + SIZE;
+		rect.left = number * SIZE;		// 左右を切り分け
+		rect.right = rect.left + SIZE;
 	}
 	else
 	{
-		_rect.top = 0U;
-		_rect.bottom = SIZE;				// 上下の切り分け
+		rect.top = 0U;
+		rect.bottom = SIZE;				// 上下の切り分け
 
 		// 左右の切り分け処理
-		_rect.left = num * SIZE;			// 左右を切り分け
-		_rect.right = _rect.left + SIZE;
+		rect.left = num * SIZE;			// 左右を切り分け
+		rect.right = rect.left + SIZE;
 	}
 
-	return _rect;
+	return rect;
 }
