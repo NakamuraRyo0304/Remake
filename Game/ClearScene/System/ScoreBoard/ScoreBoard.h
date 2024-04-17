da15726f0bd6b6d7867be8e02ef931d5f956f1bc
NakamuraRyo0304 <shino.nakiri@gmail.com>
2024-04-17 10:38:04 +0900

    クリアシステムの記述を変更

--- Game/ClearScene/System/ScoreBoard/ScoreBoard.h
+++ Game/ClearScene/System/ScoreBoard/ScoreBoard.h
@@ -12,55 +12,6 @@
 class DrawNumber;
 class ScoreBoard
 {
-private:
-
-	// 数字を描画する
-	std::unique_ptr<DrawNumber> m_nums[2];
-
-	// 座標
-	DirectX::SimpleMath::Vector2 m_position;
-
-	// 色
-	DirectX::SimpleMath::Vector4 m_color;
-
-	// 数値
-	int m_number;
-
-private:
-
-	// １スプライトのサイズ
-	static const UINT SIZE;
-
-public:
-
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	ScoreBoard();
-	~ScoreBoard();
-
-	/// <summary>
-	/// 初期化関数
-	/// </summary>
-	/// <param name="pos">座標</param>
-	/// <param name="color">文字色</param>
-	/// <param name="rate">文字拡大率</param>
-	/// <param name="scRate">画面拡大率</param>
-	/// <param name="span">文字間隔</param>
-	/// <returns>なし</returns>
-	void Initialize(DirectX::SimpleMath::Vector2 pos,
-		DirectX::SimpleMath::Vector4 color, DirectX::SimpleMath::Vector2 rate,
-		DirectX::SimpleMath::Vector2 scRate, double span);
-
-	/// <summary>
-	/// 描画関数
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	void Draw();
-
 public:
 
 	// 座標を取得
@@ -73,6 +24,34 @@ public:
 	void SetColor(const DirectX::SimpleMath::Vector4& color) { m_color = color; }
 	// 数値を設定
 	void SetNumber(const int& num) { m_number = num; }
+
+	// コンストラクタ
+	ScoreBoard();
+	// デストラクタ
+	~ScoreBoard();
+	// 初期化
+	void Initialize(DirectX::SimpleMath::Vector2 pos,
+		DirectX::SimpleMath::Vector4 color, DirectX::SimpleMath::Vector2 rate,
+		DirectX::SimpleMath::Vector2 scRate, double span);
+	// 描画
+	void Draw();
+
+private:
+
+	// １スプライトのサイズ
+	static const UINT SIZE;
+
+private:
+
+	// 数字を描画する
+	std::unique_ptr<DrawNumber> m_nums[2];
+	// 座標
+	DirectX::SimpleMath::Vector2 m_position;
+	// 色
+	DirectX::SimpleMath::Vector4 m_color;
+	// 数値
+	int m_number;
+
 };
 
 #endif // SCOREBOARD
--- Game/ClearScene/System/UI_Clear/UI_Clear.h
+++ Game/ClearScene/System/UI_Clear/UI_Clear.h
@@ -9,16 +9,14 @@
 #ifndef UI_CLEAR
 #define UI_CLEAR
 
-//==============================================================================
 // 親シーンクラス
-//==============================================================================
 #include "Game/Common/IUserInterface/IUserInterface.h"
 
 class UI_Clear final : public IUserInterface
 {
 public:
 
-	// 選択
+	// 選択の種類
 	enum SELECT { NEXT, RESTART, STAGES, LENGTH };
 
 	// スプライトに対応する構造体
@@ -28,60 +26,44 @@ public:
 		DirectX::SimpleMath::Vector4 color;
 	};
 
-private:
+public:
 
-	// オプション
-	std::map<const wchar_t*, Option> m_options;
+	// 次の操作を取得
+	SELECT GetSelecion() const { return m_select; }
+	// 次の操作を設定
+	void SetSelection(const SELECT& selection) { m_select = selection; }
 
-	// 選択
-	SELECT m_select;
+	// 動作の終了状況を取得する
+	bool IsEndMoving() const { return is_endMoving; }
 
-	// 動作終了フラグ
-	bool is_endMoving;
-
-private:
-
-	// スプライト
-	std::unique_ptr<DrawSprite> m_sprites;
+	// コンストラクタ
+	UI_Clear(DirectX::SimpleMath::Vector2 scS, DirectX::SimpleMath::Vector2 mscs);
+	// デストラクタ
+	~UI_Clear();
+	// 初期化
+	void Initialize() override;
+	// 更新
+	void Update() override;
+	// 描画
+	void Draw() override;
 
 private:
 
 	// 色変更速度
 	static const float COLOR_SPEED;
-
 	// 最終X座標
 	static const float LAST_POS_X;
 
-public:
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	/// <param name="scS">画面サイズ</param>
-	/// <param name="mscS">最大サイズ</param>
-	/// <returns>なし</returns>
-	UI_Clear(DirectX::SimpleMath::Vector2 scS, DirectX::SimpleMath::Vector2 mscs);
-	~UI_Clear();
+private:
 
-	/// <summary>
-	/// 初期化関数
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	void Initialize() override;
-
-	/// <summary>
-	/// 更新処理
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	void Update() override;
-
-	/// <summary>
-	/// 描画処理
-	/// </summary>
-	/// <param name="引数無し"></param>
-	/// <returns>なし</returns>
-	void Draw() override;
+	// スプライト
+	std::unique_ptr<DrawSprite> m_sprites;
+	// オプション
+	std::map<const wchar_t*, Option> m_options;
+	// 選択
+	SELECT m_select;
+	// 動作終了フラグ
+	bool is_endMoving;
 
 private:
 
@@ -92,16 +74,6 @@ private:
 	/// <returns>なし</returns>
 	void ChangeColor(const SELECT& select);
 
-public:
-
-	// 次の操作を取得
-	SELECT GetSelecion() const { return m_select; }
-
-	// 次の操作を設定
-	void SetSelection(const SELECT& selection) { m_select = selection; }
-
-	// 動作の終了状況を取得する
-	bool IsEndMoving() const { return is_endMoving; }
 };
 
 #endif // UI_CLEAR
